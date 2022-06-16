62693d5830a7dbff0a1d3db168301f485e28cd08
Kamilla Antunes <kamilla.antunes@mercadolivre.com>
2022-06-16 16:00:33 -0300

    Resolving Exercise 1

--- AlkeParking.playground/Contents.swift
+++ AlkeParking.playground/Contents.swift
@@ -1,5 +1,7 @@
 import Foundation
 import Darwin
+
+//Exercício 2
 enum VehicleType {
     case car
     case moto
@@ -26,9 +28,11 @@ struct Vehicle : Parkable, Hashable {
     let type: VehicleType
     var checkInTime: Date
     var discountCard: String?
-    var parkedTime: Int?
-
-
+    var parkedTime: Int {
+        get {
+            Calendar.current.dateComponents([.minute], from: checkInTime, to: Date()).minute ?? 0
+        }
+    }
 
     func hash(into hasher: inout Hasher) {
         hasher.combine(plate.hashValue)
@@ -37,122 +41,127 @@ struct Vehicle : Parkable, Hashable {
     static func == (lhs: Vehicle, rhs: Vehicle) -> Bool {
         lhs.plate == rhs.plate
     }
-
-    func checkOutVehicle(plate: String, onSuccess: (Int) -> Void, onError: ()) {
-        guard alkeParking.vehicles.contains(self) else
-            {
-            return onError
-            }
-        let veiculoRemover = self
-        alkeParking.vehicles.remove(veiculoRemover)
-        onSuccess(2)
-    }
 }
 
 struct Parking {
     var vehicles: Set<Vehicle> = Set()
-    let quantidadeMaximaVeiculos = 3
+    let maxVehicles = 20
+    // Exercício 11
+    var dataParking: (vehiclesRemoved: Int, earnings: Int) = (0,0)
+    let onFinish: (Bool) -> Void = {$0 ? print("Welcome to AlkeParking!") : print("Sorry, the check-in failed")}
 
-    mutating func checkInVehicle(_ vehicle: Vehicle, onFinish:
-    (Bool) -> Void) {
+    // Exercício 5
+    mutating func checkInVehicle(_ vehicle: Vehicle, onFinish:(Bool) -> Void) {
+        guard vehicles.count < maxVehicles else { return onFinish(false) }
 
-        guard vehicles.count < quantidadeMaximaVeiculos else {
-            return onFinish(false)
-
-        }
-
-        guard !vehicles.contains(vehicle) else {
-            return onFinish(false)
-
-        }
+        guard !vehicles.contains(vehicle) else { return onFinish(false) }
 
         onFinish(vehicles.insert(vehicle).inserted)
     }
 
-    func onFinished(aceito: Bool){
-        if aceito {
-            print("Welcome to AlkeParking!")
-        } else {
-            print("Sorry, the check-in failed")
+    // Exercício 7
+    mutating func checkOutVehicle(plate: String, onSuccess: (Int) -> Void, onError: () -> Void) {
+        var vehicleRemoveOptional: Vehicle?
+
+        for vehicle in vehicles {
+            if vehicle.plate.elementsEqual(plate){
+                vehicleRemoveOptional = vehicle
+                break
+            }
         }
+
+        guard let vehicleRemove = vehicleRemoveOptional else { return onError() }
+        vehicles.remove(vehicleRemove)
+        dataParking.vehiclesRemoved += 1
+
+        let hasDiscountCard = vehicleRemove.discountCard == nil ? false : true
+
+        // Exercício 10
+        let fee = calculateFee(vehicle: vehicleRemove, parkedTime: vehicleRemove.parkedTime, hasDiscountCard: hasDiscountCard)
+
+        dataParking.earnings += fee
+
+        onSuccess(fee)
     }
 
-    func calculateFee(vehicle: Vehicle, parkedTime: Int) -> Int {
+    // Exercício 8 e 9
+    func calculateFee(vehicle: Vehicle, parkedTime: Int, hasDiscountCard: Bool) -> Int {
+        let fixedFee = vehicle.type.fee
+        let variableFee = Int(ceil(Double((parkedTime - 120)) / 15.0) * 5)
 
-        if parkedTime <= 120 {
-            return vehicle.type.fee
-        } else {
-            return vehicle.type.fee + ((parkedTime - 120)/15 * 5)        }
+        let fee = parkedTime <= 120 ? fixedFee : fixedFee + variableFee
+
+        return hasDiscountCard ? fee - Int(Double(fee) * 0.15) : fee
+    }
+
+    // Exercício 11
+    func consulteDataParking() {
+        print("\(dataParking.vehiclesRemoved) vehicles have checked out and have earnings of $\(dataParking.earnings)")
+    }
+
+    // Exercício 12
+    func listVehicles() {
+        for vehicle in vehicles {
+            print("Plate: \(vehicle.plate)")
+        }
     }
 }
 
 protocol Parkable {
     var plate: String { get }
     var type: VehicleType { get }
+    //Exercício 3
     var checkInTime: Date { get }
     var discountCard: String? { get }
-    var parkedTime: Int? { get }
-
-    func checkOutVehicle(plate: String, onSuccess:
-                         (Int)->Void, onError:())
-
+    //Exercício 4
+    var parkedTime: Int { get }
 }
-
-let estacionamento: Parking = Parking()
 
 var alkeParking = Parking()
 
-let vehicle1 = Vehicle(plate: "AA111AA", type:
-VehicleType.car, checkInTime: Date(), discountCard:"DISCOUNT_CARD_001")
-
-let vehicle2 = Vehicle(plate: "B222BBB", type:
-VehicleType.moto, checkInTime: Date(), discountCard: nil)
-
-let vehicle3 = Vehicle(plate: "CC333CC", type:
-VehicleType.miniBus, checkInTime: Date(), discountCard:nil)
-
-let vehicle4 = Vehicle(plate: "DD444DD", type:
-VehicleType.bus, checkInTime: Date(), discountCard:"DISCOUNT_CARD_002")
-
-let vehicle5 = Vehicle(plate: "AA111BB", type:
-VehicleType.car, checkInTime: Date(), discountCard:"DISCOUNT_CARD_003")
-
-let vehicle6 = Vehicle(plate: "B222CCC", type:
-VehicleType.moto, checkInTime: Date(), discountCard:"DISCOUNT_CARD_004")
-
-let vehicle7 = Vehicle(plate: "CC333CC", type:
-VehicleType.miniBus, checkInTime: Date(), discountCard: nil)
-
-let vehicle8 = Vehicle(plate: "DD444EE", type:
-VehicleType.bus, checkInTime: Date(), discountCard:"DISCOUNT_CARD_005")
-
-let vehicle9 = Vehicle(plate: "AA111CC", type:
-VehicleType.car, checkInTime: Date(), discountCard: nil)
-
-let vehicle10 = Vehicle(plate: "B222DDD", type:
-VehicleType.moto, checkInTime: Date(), discountCard: nil)
-
-let vehicle11 = Vehicle(plate: "CC333EE", type:
-VehicleType.miniBus, checkInTime: Date(), discountCard:nil)
-
-let vehicle12 = Vehicle(plate: "DD444GG", type:
-VehicleType.bus, checkInTime: Date(), discountCard:"DISCOUNT_CARD_006")
-
-let vehicle13 = Vehicle(plate: "AA111DD", type:
-VehicleType.car, checkInTime: Date(), discountCard:"DISCOUNT_CARD_007")
-
-let vehicle14 = Vehicle(plate: "B222EEE", type:
-VehicleType.moto, checkInTime: Date(), discountCard: nil)
-
-let vehicle15 = Vehicle(plate: "CC333FF", type:
-VehicleType.miniBus, checkInTime: Date(), discountCard:nil)
-
-var veiculos: [Vehicle] = [
-    vehicle1,
-    vehicle2,
-    vehicle3
+var vehicles: [Vehicle] = [
+    Vehicle(plate: "AA111AA", type: VehicleType.car, checkInTime: Date(), discountCard:"DISCOUNT_CARD_001"),
+    Vehicle(plate: "B222BBB", type:VehicleType.moto, checkInTime: Date(), discountCard: nil),
+    Vehicle(plate: "CC333CC", type: VehicleType.miniBus, checkInTime: Date(), discountCard:nil),
+    Vehicle(plate: "DD444DD", type: VehicleType.bus, checkInTime: Date(), discountCard:"DISCOUNT_CARD_002"),
+    Vehicle(plate: "AA111BB", type: VehicleType.car, checkInTime: Date(), discountCard:"DISCOUNT_CARD_003"),
+    Vehicle(plate: "B222CCC", type: VehicleType.moto, checkInTime: Date(), discountCard:"DISCOUNT_CARD_004"),
+    Vehicle(plate: "CC333CC", type: VehicleType.miniBus, checkInTime: Date(), discountCard: nil),
+    Vehicle(plate: "DD444EE", type: VehicleType.bus, checkInTime: Date(), discountCard:"DISCOUNT_CARD_005"),
+    Vehicle(plate: "AA111CC", type: VehicleType.car, checkInTime: Date(), discountCard: nil),
+    Vehicle(plate: "B222DDD", type: VehicleType.moto, checkInTime: Date(), discountCard: nil),
+    Vehicle(plate: "CC333EE", type: VehicleType.miniBus, checkInTime: Date(), discountCard:nil),
+    Vehicle(plate: "DD444GG", type: VehicleType.bus, checkInTime: Date(), discountCard:"DISCOUNT_CARD_006"),
+    Vehicle(plate: "AA111DD", type: VehicleType.car, checkInTime: Date(), discountCard:"DISCOUNT_CARD_007"),
+    Vehicle(plate: "B222EEE", type: VehicleType.moto, checkInTime: Date(), discountCard: nil),
+    Vehicle(plate: "CC333FF", type: VehicleType.miniBus, checkInTime: Date(), discountCard:nil),
+    Vehicle(plate: "DD444FF", type: VehicleType.bus, checkInTime: Date(), discountCard:"DISCOUNT_CARD_008"),
+    Vehicle(plate: "AA111FF", type: VehicleType.car, checkInTime: Date(), discountCard:"DISCOUNT_CARD_009"),
+    Vehicle(plate: "BB222FF", type: VehicleType.moto, checkInTime: Date(), discountCard:"DISCOUNT_CARD_010"),
+    Vehicle(plate: "CC333GG", type: VehicleType.miniBus, checkInTime: Date(), discountCard:nil),
+    Vehicle(plate: "DD444HH", type: VehicleType.bus, checkInTime: Date(), discountCard:"DISCOUNT_CARD_011"),
+    Vehicle(plate: "AA111GG", type: VehicleType.car, checkInTime: Date(), discountCard:nil),
+    Vehicle(plate: "BB222GG", type: VehicleType.car, checkInTime: Date(), discountCard:nil)
 ]
 
-for veiculo in veiculos {
-    alkeParking.checkInVehicle(veiculo, onFinish: alkeParking.onFinished(aceito:))
+
+
+// Exercício 6
+for vehicle in vehicles {
+    alkeParking.checkInVehicle(vehicle, onFinish: alkeParking.onFinish)
 }
+
+print("\n")
+alkeParking.listVehicles()
+
+print("\n")
+
+let onSuccess: (Int) -> Void = {print("Your fee is $\($0). Come back soon")}
+let onError: () -> Void = {print("Sorry, the check-out failed")}
+
+for vehicle in vehicles {
+    alkeParking.checkOutVehicle(plate: vehicle.plate, onSuccess: onSuccess, onError: onError)
+}
+
+print("\n")
+alkeParking.consulteDataParking()
